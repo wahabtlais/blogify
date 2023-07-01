c2bc7a2092b5f8d3b3dd09fc636d06f563f23eb6
Wahab Tlais <hussientlais20@gmail.com>
2023-07-01 15:46:39 +0300

    Title Updated

--- Blogify/partials/header.php
+++ Blogify/partials/header.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['user-id'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Egator Blog</title>
+    <title>Blogify</title>
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" href="<?= ROOT_URL ?>css/style.css" />
     <!-- ICONSCOUT CDN -->
